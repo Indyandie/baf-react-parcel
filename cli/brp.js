8829e172d8e0b089e265b3eaaac5aebfc10a1915
Andy Contreras <andy.j.contreras@me.com>
2020-09-20 16:57:20 -0500

    stdio and clean up

--- cli/brp.js
+++ cli/brp.js
@@ -9,27 +9,19 @@ const dirname = process.argv[2] || 'barp'
 const spawner = (cmd, args=[], exit) => {
    const cmdName = `${cmd} ${args.length > 0 ? args[0] : ''}`
    
-   let spawny = ''
+   let spawny
 
-   if(args)  spawny = spawn(cmd, args)
-   else spawny = spawn(cmd)
+   if(args)  spawny = spawn(cmd, args, {stdio: 'inherit'})
+   else spawny = spawn(cmd, [], {stdio: 'inherit'})
 
-   spawny.stdout.on('data', data => {
-      console.log(chalk.green(`${data}`))
-   })
-
-   spawny.stderr.on('data', data => {
-      console.error(chalk.blue.bold(`${data}`))
-   })
-
-   spawny.on('error', (error) => {
-      console.error(chalk.red(`${error}`))
-   })
-
-   spawny.on('close', (code) => {
+   spawny.on('error', 
+      error => console.error(chalk.red(error))
+   )
+   spawny.on('close', code => {
       console.log(chalk.green.bold(`✅ ${cmdName}`))
       console.log(`${'='.repeat(64)} \n`)
-      if(exit) exit()
+      if(exit && code === 0) exit()
+      else spawny.kill()
    })
 
    return spawny
@@ -43,6 +35,7 @@ const barp = async () => {
       () => {
          process.chdir(dirname)
          console.log('📦 installing packages...')
+
          spawner('rm', ['-rf', '.npmignore', 'cli', '.git'], 
             () => {
                spawner('git', ['init'], 
@@ -62,15 +55,17 @@ const barp = async () => {
 }
 
 // Check if file exist
+let isDuplicate
+
 const list = spawn('ls')
-let isDuplicate = false
-
-list.stdout.on('data', data => {
-   const files = new Set(`${data}`.split('\n'))
-   isDuplicate = files.has(dirname)
-})
-
-list.on('close', (code) => {
-   if(!isDuplicate) barp()
-   else console.log(chalk.red(`\nThe directory <${chalk.bold(dirname)}> is in use.\n`), chalk.blueBright('🤖 Try a different name.'))
-})
+   .stdout.on('data', data => {
+      const files = new Set(`${data}`.split('\n'))
+      isDuplicate = files.has(dirname)
+   })
+   .on('close', code => {
+      if(!isDuplicate) barp()
+      else console.log(
+         chalk.red(`\nThe directory <${chalk.bold(dirname)}> is in use.\n`), 
+         chalk.blueBright('🤖 Try a different name.')
+      )
+   })
