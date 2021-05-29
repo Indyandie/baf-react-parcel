936ad2cc2c1bf0b318cd8c32c1c7ef12c42ca31b
Andy Contreras <andy.j.contreras@me.com>
2021-05-28 20:23:06 -0500

    class >> func

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Box from './Box'
 import './App.less'
 
-export default class App extends Component {
-  handleClick () {
+export default function App () {
+  const handleClick = () => {
     console.log('Hello')
   }
 
-  render() {
-    return (
-      <main id="app">
-        <h1 className="emoji">
-          <div onClick={this.handleClick}>⚛️</div>
-          <Box />
-        </h1>
-        <h2>
-          Basic <strong>AF</strong> React Parcel
-        </h2>
-      </main>
-    )
-  }
+  return (
+    <main id="app">
+      <h1 className="emoji">
+        <div onClick={handleClick}>⚛️</div>
+        <Box />
+      </h1>
+      <h2>
+        Basic <strong>AF</strong> React Parcel
+      </h2>
+    </main>
+  )
 }
